e7907966ae0af487f4a7382cd06c83cc785d079f
jorisalen <joris.alen@student.khleuven.be>
2015-02-09 20:43:02 +0100

    test 2

--- Signen/src/HelloWorld.java
+++ Signen/src/HelloWorld.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		JOptionPane.showConfirmDialog(null, "LOLOLOLOLOLOLOLOLOLOLOL");
+		JOptionPane.showConfirmDialog(null, "LELELELLELELELELLELE");
 		try{
 			System.getProperty("user.dir");
 			JOptionPane.showMessageDialog(null, "gelukt!");
