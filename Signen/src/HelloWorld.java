66f5755fd469df211e1c62670d46899441cf460c
jorisalen <joris.alen@student.khleuven.be>
2015-02-09 21:47:23 +0100

    teyqzgediqaz

--- Signen/src/HelloWorld.java
+++ Signen/src/HelloWorld.java
@@ -11,7 +11,7 @@ public class HelloWorld extends HttpServlet {
   public void init() throws ServletException
   {
       // Do required initialization
-      message = "Hello World";
+      message = "Hello csgo";
   }
 
   public void doGet(HttpServletRequest request,
